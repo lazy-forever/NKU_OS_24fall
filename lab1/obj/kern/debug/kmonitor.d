d22ab42d8ecf5150100543a9a0a640e9482f7ed2
KaiserTT <kaisertan015@gmail.com>
2024-09-25 16:10:44 +0800

    delete obj and bin

--- lab1/obj/kern/debug/kmonitor.d
+++
@@ -1,4 +0,0 @@
-obj/kern/debug/kmonitor.o obj/kern/debug/kmonitor.d: \
- kern/debug/kmonitor.c kern/debug/kdebug.h libs/defs.h \
- kern/debug/kmonitor.h kern/trap/trap.h libs/stdio.h libs/stdarg.h \
- libs/string.h
--- lab1/obj/kern/mm/pmm.d
+++
@@ -1,1 +0,0 @@
-obj/kern/mm/pmm.o obj/kern/mm/pmm.d: kern/mm/pmm.c kern/mm/pmm.h
